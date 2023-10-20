58d434c6ab0c81004940e17bb28c639a856dffeb
jishan1019 <imranislamjishan200@gmail.com>
2023-10-20 12:43:27 +0600

    download spreed show on chart add

---
+++ js/ipinfo.js
@@ -0,0 +1,37 @@
+const resultsContainer = document.getElementById("results");
+const loadingContainer = document.getElementById("loading");
+
+const ipInfoUrl = "https://ipinfo.io?token=2bc4e4d26fc693";
+
+function updateIPInfo() {
+  loadingContainer.classList.remove("hidden");
+
+  // fetch IP info
+  fetch(ipInfoUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      resultsContainer.innerHTML += `
+        
+           <p class="mt-4">IP: ${data?.ip}</p>
+            <p>City: ${data?.city}</p>
+            <p>Region: ${data?.region}</p>
+            <p>Country: ${data?.country}</p>
+            <p>Location: ${data?.loc}</p>
+            <p>Organization: ${data?.org}</p>
+            <p>Postal: ${data?.postal}</p>
+            <p>Timezone: ${data?.timezone}</p>
+        
+          `;
+      loadingContainer.classList.add("hidden");
+    })
+    .catch((error) => {
+      console.error("Error fetching IP information:", error);
+      loadingContainer.classList.add("hidden"); // Hide loading spinner
+    });
+}
+
+async function runSpeedTest() {
+  resultsContainer.innerHTML = "Running speed test...";
+}
+
+updateIPInfo();
--- js/spreed.js
+++ js/spreed.js
@@ -1,29 +1,84 @@
-const resultsContainer = document.getElementById("results");
-const loadingContainer = document.getElementById("loading");
+const ctx = document.getElementById("speedChart").getContext("2d");
+const currentSpeedElement = document.getElementById("currentSpeed");
+const averageSpeedElement = document.getElementById("averageSpeed");
 
-const ipInfoUrl = "https://ipinfo.io?token=2bc4e4d26fc693";
+const config = {
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "Download Speed (Mbps)",
+        data: [],
+        borderColor: "rgba(75, 192, 192, 1)",
+        fill: false,
+      },
+      {
+        label: "Upload Speed (Mbps)",
+        data: [],
+        borderColor: "rgba(192, 75, 192, 1)",
+        fill: false,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      x: {
+        display: true,
+        title: {
+          display: true,
+          text: "Time",
+        },
+      },
+      y: {
+        beginAtZero: true,
+        display: true,
+        title: {
+          display: true,
+          text: "Speed (Mbps)",
+        },
+      },
+    },
+  },
+};
 
-function updateIPInfo() {
-  loadingContainer.classList.remove("hidden");
+const speedChart = new Chart(ctx, config);
 
-  // fetch IP info
-  fetch(ipInfoUrl)
-    .then((res) => res.json())
-    .then((data) => {
-      resultsContainer.innerHTML += `
-        
-           <p class="mt-4">IP: ${data?.ip}</p>
-            <p>City: ${data?.city}</p>
-            <p>Region: ${data?.region}</p>
-            <p>Country: ${data?.country}</p>
-            <p>Location: ${data?.loc}</p>
-            <p>Organization: ${data?.org}</p>
-            <p>Postal: ${data?.postal}</p>
-            <p>Timezone: ${data?.timezone}</p>
-        
-          `;
-      loadingContainer.classList.add("hidden");
-    });
-}
+let time = 0;
+let totalDownloadSpeed = 0;
+let totalUploadSpeed = 0;
+let dataPoints = 0;
 
-updateIPInfo();
+setInterval(() => {
+  time += 5;
+  const downloadSpeed = Math.random() * 100;
+  const uploadSpeed = Math.random() * 100;
+
+  config.data.labels.push(time.toString());
+  config.data.datasets[0].data.push(downloadSpeed);
+  config.data.datasets[1].data.push(uploadSpeed);
+
+  // Keep a maximum of 20 data points
+  if (config.data.labels.length > 20) {
+    config.data.labels.shift();
+    config.data.datasets[0].data.shift();
+    config.data.datasets[1].data.shift();
+  }
+
+  speedChart.update();
+
+  // Calculate average speed
+  totalDownloadSpeed += downloadSpeed;
+  totalUploadSpeed += uploadSpeed;
+  dataPoints++;
+
+  const averageDownloadSpeed = totalDownloadSpeed / dataPoints;
+  const averageUploadSpeed = totalUploadSpeed / dataPoints;
+
+  currentSpeedElement.textContent = `Current Download Speed: ${downloadSpeed.toFixed(
+    2
+  )} Mbps, Upload Speed: ${uploadSpeed.toFixed(2)} Mbps`;
+  averageSpeedElement.textContent = `Average Download Speed: ${averageDownloadSpeed.toFixed(
+    2
+  )} Mbps, Average Upload Speed: ${averageUploadSpeed.toFixed(2)} Mbps`;
+}, 5000);
